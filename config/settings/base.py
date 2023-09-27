b6dbad9cf479404a0134e9278f21ec3855f3c7e9
Julien Schoener <julien.schoener@gmail.com>
2023-09-27 10:59:24 +0200

    added image

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -87,3 +88,7 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "user.User"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
